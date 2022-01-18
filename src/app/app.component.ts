7d304f9eca16db73869d49a887a6b9f6ced0231f
amit yadav <49114459+amiy007@users.noreply.github.com>
2022-01-18 12:28:26 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'employee-detail-crud';
+  title = 'Angular 8 + Spring Boot 2 CRUD Tutorial';
 }
